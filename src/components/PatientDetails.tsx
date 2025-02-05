2a92a3d449d46c032b147ad444249b443b6d9ee6
Joel German <joelgerman671@gmail.com>
2025-02-05 02:56:00 -0400

    get id to patient to implement edit funcionality and implement devtools middleware of zustand to see our state on redux devtools extension chrome

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -1,6 +1,7 @@
 import { Patient } from '../types/index';
 import { PatientDetailItem } from './PatientDetailItem';
 import { usePatientStore } from '../store';
+
 type PatientDetailsProps = {
     patient: Patient
 }
@@ -8,6 +9,7 @@ type PatientDetailsProps = {
 export const PatientDetails = ({patient} : PatientDetailsProps) => {
 
   const deletePatient = usePatientStore(state => state.deletePatient)
+  const getPatientById = usePatientStore(state => state.getPatientById)
   
   return (
     <div className='mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-lg'>
@@ -18,8 +20,10 @@ export const PatientDetails = ({patient} : PatientDetailsProps) => {
        <PatientDetailItem  label='Fecha de Alta' data={patient.date.toString()}/>
        <PatientDetailItem  label='Sintomas' data={patient.symptoms}/>
 
-       <div className="flex justify-between gap-3 mt-10">
-        <button className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold rounded-lg uppercase">Editar
+       <div className="flex flex-col lg:flex-row justify-between gap-3 mt-10">
+        <button className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold rounded-lg uppercase"
+        onClick={() => getPatientById(patient.id)}
+        >Editar
         </button>
         <button 
           className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold rounded-lg uppercase"
--- src/store.ts
+++ src/store.ts
@@ -2,20 +2,25 @@
 import { create } from "zustand";
 import { DraftPatient, Patient } from './types/index';
 import { v4 as uuid } from "uuid";
+import { devtools } from "zustand/middleware";
+
 
 type PatientState = {
     patients: Patient[]
+    activeId: string
     addPatient: (data: DraftPatient) => void
     deletePatient: (id: Patient['id']) => void
+    getPatientById: (id: Patient['id']) => void
 }
 
 const createPatient = (patient: DraftPatient) : Patient => {
     return {...patient, id: uuid()}
 }
 
-export const usePatientStore = create<PatientState>((set) => ({
+export const usePatientStore = create<PatientState>()(
+    devtools((set) => ({
     patients: [],
-
+    activeId: '',
     addPatient: (data) => {
         set((state)=>({
             patients: [...state.patients, createPatient(data)]
@@ -27,6 +32,15 @@ export const usePatientStore = create<PatientState>((set) => ({
         set((state)=>({
             patients: state.patients.filter( patient => patient.id !== id )
         }))
+    },
+
+    getPatientById: (id) => {
+        console.log(id);
+        set(()=>({
+            activeId : id
+        }))
+
+
     }
 
-}))
+})))
