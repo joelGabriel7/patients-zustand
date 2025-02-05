ff58691e48e74135051f0dd3dbd917b5e833b826
Joel German <joelgerman671@gmail.com>
2025-02-05 02:30:34 -0400

    Show data of patientes and separate on different component to reusability

--- src/components/PatientList.tsx
+++ src/components/PatientList.tsx
@@ -1,6 +1,35 @@
+import { usePatientStore } from "../store"
+import { PatientDetails } from "./PatientDetails";
 
 export const PatientList = () => {
+  const  patients = usePatientStore(state => state.patients)
   return (
-    <div>PatientList</div>
+    <div className="md:w-1/2 lg:3/5 md:h-screen overflow-y-scroll">
+        { patients.length ? (
+          <>
+            <h2 className="font-black text-3xl text-center">Listado de Pacientes</h2>
+             <p className="text-xl mt-5 mb-10 text-center">
+              Administras tus {''}
+              <span className="text-indigo-600 font-bold">y apareceran en este lugar</span>
+             </p>
+
+             {patients.map( patient => (
+              <PatientDetails 
+                key={patient.id}
+                patient={patient}
+              />
+             ))}
+          </>
+        ) : (
+          <>
+              <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+              <p className="text-xl mt-5 mb-10 text-center">
+                  Comienza agregando pacientes {''}
+                  <span className="text-indigo-600 font-bold">y apareceran en este lugar</span>
+              </p>
+
+          </>
+        )}
+    </div>
   )
 }
