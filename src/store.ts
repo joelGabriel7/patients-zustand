6083acd771046f2b79e3ae2f5690e9ef7ec722ca
Joel German <joelgerman671@gmail.com>
2025-02-05 02:08:08 -0400

    Setting up data to addPatient and generic id with uuid depency

--- src/store.ts
+++ src/store.ts
@@ -1,15 +1,22 @@
 
 import { create } from "zustand";
 import { DraftPatient, Patient } from './types/index';
+import { v4 as uuid } from "uuid";
 
 type PatientState = {
     patients: Patient[]
     addPatient: (data: DraftPatient) => void
 }
 
-export const usePatientStore = create<PatientState>(() => ({
+const createPatient = (patient: DraftPatient) : Patient => {
+    return {...patient, id: uuid()}
+}
+
+export const usePatientStore = create<PatientState>((set) => ({
     patients: [],
     addPatient: (data) => {
-        console.log(data);
+        set((state)=>({
+            patients: [...state.patients, createPatient(data)]
+        }))
     }
 }))
